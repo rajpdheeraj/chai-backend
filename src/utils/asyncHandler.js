507be898210e251e9276e9cb55624c5d9be1c8f6
Dheeraj Kumar <mthdheerajkumar@gmail.com>
2024-02-19 11:32:25 +0530

    all setup configured

--- src/utils/asyncHandler.js
+++ src/utils/asyncHandler.js
@@ -1,4 +1,5 @@
-const asyncHandler = (requestHandler) => {(req , res , next)=>{
+const asyncHandler = (requestHandler) => {
+  return (req , res , next)=>{
   process.resolve(requestHandler(req , res , next)).catch((err)=> next(err))
 }}
 
