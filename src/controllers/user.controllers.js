392d037db972504c8e27b0f6d733e6b7a9822295
Dheeraj Kumar <mthdheerajkumar@gmail.com>
2024-03-02 00:13:05 +0530

    add new routes for user controllers

--- src/controllers/user.controllers.js
+++ src/controllers/user.controllers.js
@@ -233,7 +233,7 @@ const changeCurrentPassword = asyncHandler( async (req , res) => {
 
 })
 
-const gerCurrentUser = asyncHandler(async(req, res)=>{
+const getCurrentUser = asyncHandler(async(req, res)=>{
   return res
   .status(200)
   .json(200 , req.user , "current user fetched successfully")
@@ -451,7 +451,7 @@ export {
   logoutUser,
   refreshAccessToken,
   changeCurrentPassword,
-  gerCurrentUser,
+  getCurrentUser,
   updateAccountDetails,
   updateUserAvatar,
   updateUserCoverImage,
